746d5f0612a3384ec514b2020be6681ef77754bb
demin7926 <demin7926@gmail.com>
2014-06-03 18:19:09 +0800

    UDP Server-Client demo

---
+++ udp/serv.c
@@ -0,0 +1,15 @@
+/**
+ * Program:
+ *	This is a UDP server demo
+ * History:
+ *	mandy @ 2014-06-03
+ */
+
+#include "mh.h"
+
+int main(int argc, char*argv[])
+{
+	puts("UDP Server Demo");
+
+	return 0;
+}
